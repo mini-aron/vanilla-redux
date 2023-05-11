1a419bee004e711b0ff8ceb16552bbc2520a12e0
우엥 <105274015+rnldudns@users.noreply.github.com>
2023-05-11 10:43:15 +0900

    detail page 완성

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-function ToDo({ text, onBtnClick }) {
+function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      {text} <button onClick={onBtnClick}>DEL</button>
+      <Link to={`/${id}`}>
+        {text} <button onClick={onBtnClick}>DEL</button>
+      </Link>
     </li>
   );
 }
 
-function mapDispatchToProps(dispatch, ownProps){
-    return {
-        onBtnClick: () => dispatch(actionCreators.deleteToDo(parseInt(ownProps.id)))
-    };
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onBtnClick: () =>
+      dispatch(actionCreators.deleteToDo(parseInt(ownProps.id))),
+  };
 }
 
-export default connect(null,mapDispatchToProps) (ToDo);
+export default connect(null, mapDispatchToProps)(ToDo);
